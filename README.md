542757230e2006a413f255fe3c30301ad93aec40
hpetter <henrik@hpetter.de>
2022-08-04 13:07:59 +0200

    experimented with some more inputs (for archive purposes)

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # question-answer-showcase
 
-## how to use
+Sample Deployment of a QA Model with Streamlit UI using Docker and HuggingFace
+
+---
+
+## How to setup
 
 Create a new venv (virtual environment):
 
@@ -34,8 +38,32 @@ pip install -r requirements.txt
 python3 -m streamlit run src/app.py
 ```
 
-or
+---
+
+## How to build & run the Docker Image
 
 ```
-streamlit run src/app.py
+docker build -t qa-showcase .
+```
+
+Run the Docker Image with:
+
+```
+docker run -d --rm -p 8501:8501 --name qa-showcase qa-showcase
+```
+
+You can now access the streamlit server at [http://localhost:8501](http://localhost:8501)
+
+<br/>
+
+To view the logs of the container run:
+
+```
+docker logs qa-showcase
+```
+
+To stop the container run:
+
+```
+docker stop qa-showcase
 ```
--- src/app.py
+++ src/app.py
@@ -1,20 +1,48 @@
+from zmq import NULL
 import streamlit as st
 from transformers import pipeline
 
-# function to load model from wherever you need. Sample uses 
+# function to load model from wherever you need. Sample uses
 @st.cache(allow_output_mutation=True, show_spinner=False)
 def use_model():
     model_name = "deepset/gelectra-base-germanquad"
-    question_answer_model = pipeline(model=model_name, tokenizer=model_name ,task="question-answering")
+    question_answer_model = pipeline(
+        model=model_name, tokenizer=model_name, task="question-answering"
+    )
     return question_answer_model
 
 
 # change sample input as needed
-sample_question = "Wo wohne ich?"
-sample_context = "Mein Name ist Klaus und ich lebe in Berlin"
+sample_question = (
+    "In welchem Quartal haben wir am meisten Umsatz gemacht?",
+    "Wie hat sich unser Gewinn entwickelt?",
+    "Wann haben wir am wenigsten Gewinne gemacht?",
+)
+sample_context = (
+    "Der Bericht zeigt, dass das Unternehmen auf einem guten Kurs ist. Unsere"
+    " Gewinne sind um 20% gestiegen. Allerdings haben im 3. Quartal letzten"
+    " Jahres sehr viel mehr Umsatz gemacht haben, als in den restlichen"
+    " Quartalen. Leider haben wir im 1. Quartal so gut wie keinen Umsatz"
+    " erzielt."
+)
+input_option = "In welchem Quartal haben wir am meisten Umsatz gemacht?"
 
-input_question = st.text_input("Die zu beantwortende Frage:", value=sample_question, max_chars=128)
-input_context = st.text_area(label="Der Kontext aus dem die Antwort gefunden werden soll", value=sample_context, height = 200, max_chars=10000)
+input_question = st.text_input(
+    "Frage selbst etwas oder versuche es mit der vorgegebenen Frage:",
+    value=input_option,
+    max_chars=128,
+)
+st.write("Andere mögliche Fragen könnten sein:")
+st.code("Wie hat sich unser Gewinn entwickelt?")
+input_context = st.text_area(
+    label=(
+        "Schreibe hier den Text aus dem die Antwort gefunden werden soll oder"
+        " nimm unseren Beispieltext:"
+    ),
+    value=sample_context,
+    height=200,
+    max_chars=10000,
+)
 
 # button returns a boolean value
 compute = st.button("Beantworte meine Frage")
@@ -31,6 +59,10 @@ hide_streamlit_style = """
 <style>
 #MainMenu {visibility: hidden;}
 footer {visibility: hidden;}
+.stCodeBlock div {padding-top: 2px; padding-bottom: 2px;}
+.stCodeBlock pre {padding-top: 2px; padding-bottom: 2px;}
+.stCodeBlock button {max-height: 20px;}
+p {margin-bottom: 4px; font-size: 14px}
 </style>
 
 """
